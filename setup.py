8047aecdc35147c52987be9b02d9d6d8a6d54c77
KateProxa <ekater_1997@mail.ru>
2025-01-30 05:20:38 +0300

    update

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="credit_default_analysis",
+    version="1.1b4",
+    description="Credit default analysis package",
+    author="Kate",
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+    python_requires='>=3.9',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
